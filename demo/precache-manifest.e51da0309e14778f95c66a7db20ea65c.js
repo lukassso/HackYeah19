ef96648dd66f7b3df8a5a6e7454f248f3f1dcc82
burzomir <burzomirdev@gmail.com>
2019-09-15 10:46:58 +0200

    Update demo

---
+++ demo/precache-manifest.e51da0309e14778f95c66a7db20ea65c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aea80f1873d860e5baa68820d614b9",
+    "url": "/HackYeah19/demo/index.html"
+  },
+  {
+    "revision": "ba6a96b33b48430a3590",
+    "url": "/HackYeah19/demo/static/css/2.471e4ee7.chunk.css"
+  },
+  {
+    "revision": "51cf5aae2bc7d962a9ed",
+    "url": "/HackYeah19/demo/static/css/main.cdbd207f.chunk.css"
+  },
+  {
+    "revision": "ba6a96b33b48430a3590",
+    "url": "/HackYeah19/demo/static/js/2.15fb2970.chunk.js"
+  },
+  {
+    "revision": "51cf5aae2bc7d962a9ed",
+    "url": "/HackYeah19/demo/static/js/main.542c0754.chunk.js"
+  },
+  {
+    "revision": "1231ff3aac9e9ea61a3f",
+    "url": "/HackYeah19/demo/static/js/runtime~main.f327d7b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HackYeah19/demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "72cbedbd121d82a5e4d32a48d293f420",
+    "url": "/HackYeah19/demo/static/media/plane.72cbedbd.svg"
+  }
+]);
--- demo/service-worker.js
+++ demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29a9a48f87193a3039e92828ac65f49e.js"
+  "/HackYeah19/demo/precache-manifest.e51da0309e14778f95c66a7db20ea65c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HackYeah19/demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
