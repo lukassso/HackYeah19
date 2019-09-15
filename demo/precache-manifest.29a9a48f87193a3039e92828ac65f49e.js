c3ca6fbe8b9edf1fc4c1f9a168b390a1acceaeb9
burzomir <burzomirdev@gmail.com>
2019-09-15 10:15:28 +0200

    Build demo

---
+++ demo/precache-manifest.29a9a48f87193a3039e92828ac65f49e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34bebaa1af55887af1562c54e44e08b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6a96b33b48430a3590",
+    "url": "/static/css/2.471e4ee7.chunk.css"
+  },
+  {
+    "revision": "511f1d47f40a88941c67",
+    "url": "/static/css/main.cdbd207f.chunk.css"
+  },
+  {
+    "revision": "ba6a96b33b48430a3590",
+    "url": "/static/js/2.15fb2970.chunk.js"
+  },
+  {
+    "revision": "511f1d47f40a88941c67",
+    "url": "/static/js/main.0ad8923a.chunk.js"
+  },
+  {
+    "revision": "48a50516a871e306853e",
+    "url": "/static/js/runtime~main.94134fc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "72cbedbd121d82a5e4d32a48d293f420",
+    "url": "/static/media/plane.72cbedbd.svg"
+  }
+]);
